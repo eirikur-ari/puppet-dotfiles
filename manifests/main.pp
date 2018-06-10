5d4b495ab6aee9e393a690d3555be8448657f937
Eirikur Ari Petursson <eirikur.ari@hotmail.com>
2018-06-10 17:43:28 +0000

    added epel-release, pip and pygments

--- manifests/main.pp
+++ manifests/main.pp
@@ -1,11 +1,20 @@
 node default {
 
+  package { 'epel-release':
+    ensure   => 'present',
+    provider => 'rpm',
+    source   => 'http://fedora.skyggnir.is/epel/epel-release-latest-7.noarch.rpm',
+  }
+
   package { 'vim':  ensure => 'installed' }
   package { 'tree': ensure => 'installed' }
   package { 'git':  ensure => 'installed' }
+  package { 'python-pip': ensure => 'installed', require => Package['epel-release'] }
+  package { 'pygments': ensure => 'present', provider => 'pip', require  => Package['python-pip']}
 
-  file { '/etc/ssh/ssh_known_hosts':
+  file { 'ssh_known_hosts':
     ensure  => 'present',
+    path    => '/etc/ssh/ssh_known_hosts',
     replace => 'no',
     mode    => '0644'
   }
@@ -13,7 +22,7 @@ node default {
   exec { 'add_github_to_known_hosts':
     path    => '/usr/bin',
     command => 'ssh-keyscan -t rsa github.com >> /etc/ssh/ssh_known_hosts',
-    require => File['/etc/ssh/ssh_known_hosts']
+    require => File['ssh_known_hosts']
   }
 
   vcsrepo { '/usr/local/share/dotfiles':
@@ -21,6 +30,6 @@ node default {
     provider          => git,
     source            => 'git@github.com:eirikur-ari/dotfiles.git',
     trust_server_cert => true,
-    require           => [ Package['git'], Exec['add_github_to_known_hosts'] ]
+    require           => [ Package['git'], Package['pygments'] , Exec['add_github_to_known_hosts'] ]
   }
 }
