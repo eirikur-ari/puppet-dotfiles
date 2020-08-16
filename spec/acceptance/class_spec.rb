20c4c1fa2ddcdf0a9f5c499a8fa9e5ce2f7a5906
Eirikur Ari Petursson <eirikur.ari@hotmail.com>
2020-08-16 22:44:13 +0200

    added new acceptance test

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -11,8 +11,8 @@ describe 'dotfiles' do
       apply_manifest(manifest, :catch_failures => true)
     end
   
- #   it 'should apply a second time without changes' do
- #     @result = apply_manifest(manifest)
- #     expect(@result.exit_code).to be_zero
- #   end
+    it 'should apply a second time without changes' do
+      @result = apply_manifest(manifest)
+      expect(@result.exit_code).to be_zero
+    end
 end
